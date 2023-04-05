0ee60981545651861d2422a4e972c4c5b7d5c377
Pshenichka1 <pshenichka.olga@gmail.com>
2023-04-06 00:16:02 +0300

    js-hw-11

--- src/fetchPixabay.js
+++ src/fetchPixabay.js
@@ -1,8 +1,27 @@
-export function fetchPixabay(searchQueryForm) {
-    const url = `https://pixabay.com/api/?key=35017734-fed2e09b3d8a04f799b9cec3a&q=${searchQueryForm}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
-    return fetch(url).then(response => {
-        if (response.ok) {
-            return response.json();
-        } throw new Error(response.statusText);
-    });
+export default class FetchPixabay {
+    constructor() {
+        this.searchQueryForm = '';
+        this.page = 1;
+     }
+    fetchArticls() {
+        const url = `https://pixabay.com/api/?key=35017734-fed2e09b3d8a04f799b9cec3a&q=${this.searchQueryForm}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+        return fetch(url).then(response => response.json()).then(({hits}) => {
+console.log(hits)
+            this.incrementPage();
+            return hits;
+           
+        })
+    }
+    incrementPage() {
+        this.page += 1;
+    }
+    resetPage() {
+        this.page = 1;
+    }
+    get query() {
+        return this.searchQueryForm
+    }
+    set query(newQuery) {
+        this.searchQueryForm = newQuery
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
 
-// import templateFunction from './template.hbs';
+import hitsCards from './templates/card-photos.hbs'
 
 // import axios from "axios";
 // import SimpleLightbox from "simplelightbox";
 // import "simplelightbox/dist/simple-lightbox.min.css";
-// import Notiflix from "notiflix";
-import { fetchPixabay } from "./fetchPixabay";
+import Notiflix from "notiflix";
+import FetchPixabay from "./fetchPixabay";
 
 const searchForm = document.querySelector('#search-form');
 const btnSubmit = document.querySelector('button');
 const galleryPhotos = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
+const newsApi = new FetchPixabay();
 // document.body.innerHTML = templateFunction();
 searchForm.addEventListener('submit', handleCearch);
+btnLoadMore.addEventListener('click', handleLoadMore);
 
 
 function handleCearch(event) {
     event.preventDefault();
-    const searchQueryForm = event.currentTarget.elements.searchQuery.value.trim();
-    fetchPixabay(searchQueryForm)
+    clearHitsPhotos();
+    newsApi.query = event.currentTarget.elements.searchQuery.value.trim();
+    if (newsApi.query === '') {
+    return  Notiflix.Notify.warning('Please enter a search parameter')  
+    }
+    newsApi.resetPage();
+    newsApi.fetchArticls().then(appendHitsMarkup);
+}
+
+function handleLoadMore() {
+    newsApi.fetchArticls().then(appendHitsMarkup);
+}
+function appendHitsMarkup(hits) {
+galleryPhotos.insertAdjacentHTML('beforeend', hitsCards(hits))
+}
+function clearHitsPhotos() {
+    galleryPhotos.innerHTML = '';
 }
